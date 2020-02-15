bf158da36730ba121a4f0236d358c4822eb6513e
Santiago Rivera <49661456+santri94@users.noreply.github.com>
2020-02-15 18:44:48 -0500

    Everything is set still gives error ... java.sql.SQLNonTransientConnectionException: Could not create connection to database server.

--- app/src/main/java/com/example/soccerapp/MainActivity.java
+++ app/src/main/java/com/example/soccerapp/MainActivity.java
@@ -8,15 +8,18 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.logging.Level;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
     }
 
-    public void Login(View view){
+    public void Login(View view) throws Exception {
         EditText username = (EditText) findViewById(R.id.email);
         EditText pass = (EditText) findViewById(R.id.password);
 
@@ -25,7 +28,28 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), "Please Enter Email and password", Toast.LENGTH_SHORT).show();
         }
         else {
-            // Compare username and pass with the ones on your DB
+            try{
+                // Compare username and pass with the ones on your DB
+                Database.DatabaseFunctions myDatabase = new Database.DatabaseFunctions(); // connect to database
+
+                // returns a User object if there was a match if not then returns a null
+                User currentUser = myDatabase.GetUser(username.getText().toString(), pass.getText().toString());
+
+                if (currentUser != null) {
+                    Toast.makeText(getApplicationContext(), "Hi "+ currentUser.F_Name, Toast.LENGTH_SHORT).show();
+
+                }
+                else{
+                    Toast.makeText(getApplicationContext(), "Wrong Email or password", Toast.LENGTH_SHORT).show();
+                }
+
+
+
+            } catch (Exception ex) {
+                System.out.println(ex);
+            }
+
+
         }
 
 
---
+++ app/src/main/java/com/example/soccerapp/User.java
@@ -0,0 +1,22 @@
+package com.example.soccerapp;
+
+public class User {
+
+    public String Email;
+    public String Pass;
+    public String F_Name;
+    public String L_Name;
+    public String DOB;
+
+
+    public User(String F_Name, String L_Name, String Email, String Pass, String DOB) {
+        this.F_Name = F_Name;
+        this.L_Name = L_Name;
+        this.Email = Email;
+        this.Pass = Pass;
+        this.DOB = DOB;
+
+
+    }
+
+}
