ba8aa36ec21e05da2a45b89be118325039a6a816
Santiago Rivera <49661456+santri94@users.noreply.github.com>
2020-02-15 16:31:29 -0500

    Implemented validating data not empty

--- app/src/main/java/com/example/soccerapp/MainActivity.java
+++ app/src/main/java/com/example/soccerapp/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +17,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void Login(View view){
+        EditText username = (EditText) findViewById(R.id.email);
+        EditText pass = (EditText) findViewById(R.id.password);
+
+        // validate that these fields are not empty
+        if (username.getText().toString().isEmpty() || pass.getText().toString().isEmpty() ) {
+            Toast.makeText(getApplicationContext(), "Please Enter Email and password", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            // Compare username and pass with the ones on your DB
+        }
+
 
     }
 
--- app/src/main/java/com/example/soccerapp/RegisterActivity.java
+++ app/src/main/java/com/example/soccerapp/RegisterActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -14,8 +16,26 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     public void Register(View view){
+        // Making sure all the data was entered
+        EditText name = (EditText) findViewById(R.id.name);
+        EditText lastName = (EditText) findViewById(R.id.lastName);
+        EditText DOB = (EditText) findViewById(R.id.DOB);
+        EditText username = (EditText) findViewById(R.id.emailRegistered);
+        EditText password = (EditText) findViewById(R.id.passwordRegistered);
+
+        // validate that these fields are not empty
+        if (name.getText().toString().isEmpty() || lastName.getText().toString().isEmpty() || DOB.getText().toString().isEmpty() || username.getText().toString().isEmpty()
+                || password.getText().toString().isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Please Enter all the information", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            // save Information on DB then return to main window
+            Toast.makeText(getApplicationContext(), "Registration Successful", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
         // Insert your data to database then return to main activity
-        finish();
+
 
     }
 }
